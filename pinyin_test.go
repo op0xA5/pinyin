acd88c470643b1b44806c0ada59d0e2b84e4aa36
zhujl <zhu@ranyu.net>
2022-04-02 15:23:38 +0800

    update tests

--- pinyin_test.go
+++ pinyin_test.go
@@ -1,23 +1,40 @@
 package pinyin
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+func assertEquals(t *testing.T, a, b interface{}) {
+	if !reflect.DeepEqual(a, b) {
+		t.Errorf("assert failed: %v != %v", a, b)
+	}
+}
 
 func TestEasy(t *testing.T) {
-	t.Log(Easy("套马的汉子你威武雄壮"))
+	assertEquals(t, Easy("套马的汉子你威武雄壮"), "taomadehanziniweiwuxiongzhuang")
+	assertEquals(t, Easy("奔驰的骏马像疾风一样"), "benchidejunmaxiangjifengyiyang")
 }
 
 func TestGetPinyin(t *testing.T) {
-	t.Log(Get("天安门").String())
-	t.Log(Get("天安门").StringStyle(FullUpper))
-	t.Log(Get("天安门").StringStyle(Initial))
-	t.Log(Get("天安门").StringStyle(InitialUpper))
-	t.Log(Get("天安门").StringStyle(First))
-	t.Log(Get("天安门").StringStyle(FirstUpper))
+	assertEquals(t, Get("知之为知之").String(), "zhizhiweizhizhi")
+	assertEquals(t, Get("知之为知之").StringStyle(FullUpper), "ZHIZHIWEIZHIZHI")
+	assertEquals(t, Get("知之为知之").StringStyle(Initial), "zhzhwzhzh")
+	assertEquals(t, Get("知之为知之").StringStyle(InitialUpper), "ZHZHWZHZH")
+	assertEquals(t, Get("知之为知之").StringStyle(First), "zzwzz")
+	assertEquals(t, Get("知之为知之").StringStyle(FirstUpper), "ZZWZZ")
 
 	py := Get("单和地")
+	// 单: dan chan shan
+	// 和: he hu huo
+	// 地: de di
+	assertEquals(t, py.Possibility(), 3*3*2)
+
+	results := make(map[string]int)
 	poss := py.Possibility()
-	t.Log("Possibility", poss)
 	for i := 0; i < poss; i++ {
-		t.Log(py.StringIndex(i, DefaultStyle))
+		results[py.StringIndex(i, DefaultStyle)] = 1
 	}
+
+	assertEquals(t, poss, len(results))
 }
